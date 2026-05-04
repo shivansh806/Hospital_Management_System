e1291767f0ae47886f054714dcea3aab4dc2f4fb
Shivansh Kumar <shivanshsingh806@gmail.com>
2026-05-04 09:00:07 +0530

    feat: integrate Google OAuth2 login with failure handler

--- src/main/java/com/shivansh/code/hospitalManagement/security/WebSecurityConfig.java
+++ src/main/java/com/shivansh/code/hospitalManagement/security/WebSecurityConfig.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 public class WebSecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
+    private final Oauth2SuccessHandler oauth2SuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -44,8 +45,7 @@ public class WebSecurityConfig {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .oauth2Login(oauth-> oauth.failureHandler((request, response, exception) -> {
                     log.error("Oauth error : {}"+exception.getMessage());
-                }).successHandler((request, response, authentication) -> {
-                }));
+                }).successHandler(oauth2SuccessHandler));
         return httpSecurity.build();
     }
 
