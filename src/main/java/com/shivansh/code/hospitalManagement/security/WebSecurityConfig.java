8249e6e7f5d9917a86ed9dde1bf22a8ba9a694a2
Shivansh Kumar <shivanshsingh806@gmail.com>
2026-05-04 08:39:43 +0530

    feat: integrate Google OAuth2 login with failure handler

--- src/main/java/com/shivansh/code/hospitalManagement/security/WebSecurityConfig.java
+++ src/main/java/com/shivansh/code/hospitalManagement/security/WebSecurityConfig.java
@@ -1,21 +1,32 @@
 package com.shivansh.code.hospitalManagement.security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class WebSecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
@@ -30,7 +41,11 @@ public class WebSecurityConfig {
 //                        .requestMatchers("/admin/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .oauth2Login(oauth-> oauth.failureHandler((request, response, exception) -> {
+                    log.error("Oauth error : {}"+exception.getMessage());
+                }).successHandler((request, response, authentication) -> {
+                }));
         return httpSecurity.build();
     }
 
